1d0f2e18d5d802061b9bdd93178981090930c107
athareMTU <55211309+athareMTU@users.noreply.github.com>
2019-10-22 22:00:18 -0400

    Update ScheduleMaker.java

--- src/ScheduleMaker.java
+++ src/ScheduleMaker.java
@@ -14,20 +14,21 @@ public class ScheduleMaker {
      */
 	private static ArrayList<String> getCC() {
 		ArrayList<String> courses = new ArrayList<>();
-		
     	return courses;
     }
     
     /**
      * This method will retrieve the Course from the given name and semester and add it to the 
      * global ArrayList
+     * @author Alex Hare
      * @param courseName - The name of the course to be searched for
      * @param semesterID - Semester to search in
      */
-    public void findCC( String courseName, String semesterID ) {
+    public static void findCC( String courseName, String semesterID ) {
     	try {
     		MultiMap<String, Course> allCourses = Scraper.getAllClasses(semesterID);
-    		currentCourse.addAll(allCourses.get(courseName));    		    		
+    		currentCourse.addAll(allCourses.get(courseName));
+    		System.out.println("Add course " + allCourses.get(courseName));
     	} catch ( ParseException | IOException ex ) {
     		System.out.println("Error PaseException, IOException");
     		System.exit(0);
@@ -37,18 +38,34 @@ public class ScheduleMaker {
     public static void main(String[] args) {
     	ArrayList<String> courses = new ArrayList<>(); // Store the courses from the GUI
     	// Create the arraylist of selected courses
-    	courses = getCC();
-    	    	
+    	//courses = getCC();
+    	
+    	// Testing
+    	courses.add("EE 3131");
+    	courses.add("EE 2174");
+    	
+    	for(int j = 0; j < courses.size(); j++) {
+    		try {
+    			findCC( courses.get(j), Scraper.getAllSemesters().get("Spring 2019 (View only)"));
+    		} catch (IOException ex) {
+    			
+    		}
+    	}
     	
     	// Compare each element in list for a conflict
     	for(int i = 0; i < currentCourse.size(); i++) {
     		for(int j = 0; j < currentCourse.size(); j++) {
     			if(currentCourse.get(i).conflicts(currentCourse.get(j))) {
     				// Error, classes conflict notify user
+    				System.out.println("Conflict Occured");
     				return;
     			}
     		}
     	}
+    	
+    	System.out.println(currentCourse.size());
+    	//System.out.println(currentCourse.get(0).getCRN());
+    	//System.out.println(currentCourse.get(1).getCRN());
     	
     	// Build schedule to GUI?   	
     	
