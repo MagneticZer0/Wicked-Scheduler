f2e75e37b675eb1e853cab9f207e1c287085286e
athareMTU <55211309+athareMTU@users.noreply.github.com>
2019-11-04 12:19:51 -0500

    Working conflict manager
    Schedule maker is working. Its wooorking!

--- src/ScheduleMaker.java
+++ src/ScheduleMaker.java
@@ -41,7 +41,90 @@ public class ScheduleMaker {
     	}
     }
     
-    public static void main(String[] args) {
+    public static ArrayList<Course> build(ArrayList<String> courses, String Semester) {
+    	ArrayList<Course> finalCourseList = new ArrayList<>();
+    	
+    	// Create the arraylist of selected courses
+    	//courses = getCC();
+    	
+    	/** Testing
+    	
+    	courses.add("CS3411 - Systems Programming");
+    	courses.add("EE3131 - Electronics");
+    	courses.add("CS4321 - Introduction to Algorithms"); **/
+    	
+    	for(int j = 0; j < courses.size(); j++) {
+			findCC( courses.get(j), Scraper.getAllSemesters().get(Semester));
+    	}
+    	
+    	Collections.sort(currentCourse);
+    	
+    	// Compare each element in list for a conflict
+    	for(int i = 0; i < currentCourse.size(); i++) {
+    		for(int j = 0; j < currentCourse.size(); j++) {
+    			if(i == j) {
+    				// Skip
+    				continue;
+    			}
+    			if(currentCourse.get(i).conflicts(currentCourse.get(j))) {
+    				// Error, classes conflict notify user
+    				System.out.println("Conflict Occured " + currentCourse.get(i) + " " + currentCourse.get(j));
+    				return null;
+    			}
+    		}
+    	}
+    	
+    	/**
+    	System.out.println(currentCourse.size());
+    	for(int i = 0; i < currentCourse.size(); i++) {
+    		System.out.println(currentCourse.get(i));
+    	}**/
+    	
+    	// This array will store the number of course repeats in order of sorted appearance
+    	int[] arr = new int[numCourses];
+    	int ind = 0;
+    	for(int i = 0; i < numCourses; i++) {
+    		arr[i] = 1; // At least one
+
+    		while(currentCourse.get(ind).toString().equals(currentCourse.get(ind + 1).toString())) {
+    			arr[i]++;
+    			ind++;
+    			if(ind < currentCourse.size()) {
+    				break;
+    			}
+    		}
+    		
+    		ind++;
+    	}    	
+    	
+    	// Build schedule
+    	// Go through the number of courses to have
+    	for(int i = 0; i < numCourses; i++) {
+    		// Go through the multiple times of that class
+    		for(int j = 0; j < arr[i]; j++) {
+    			// Add the first class
+    			if(i == 0) {
+    				finalCourseList.add(currentCourse.get(i+j));
+    			} else {
+    				// Add other classes
+    				if(finalCourseList.get( i - 1 ).conflicts(currentCourse.get(i + j + arr[i - 1]))) {
+    					// Conflict go to next option
+    					if(j == arr[i] - 1) {
+    						// If on the last option of a class and can't add it ERROR
+    						System.out.println("Error incombatable course: " + currentCourse.get(i + j));
+    					}
+    					continue;
+    				} else {
+    					finalCourseList.add(currentCourse.get(i + j + arr[i - 1]));
+    					break;
+    				}
+    			}
+    		}
+    	}
+    	return finalCourseList; 	
+    }
+    
+    public static ArrayList<Course> main(String[] args) {
     	ArrayList<String> courses = new ArrayList<>(); // Store the courses from the GUI
     	ArrayList<Course> finalCourseList = new ArrayList<>();
     	
@@ -77,10 +160,11 @@ public class ScheduleMaker {
     		}
     	}
     	
+    	/**
     	System.out.println(currentCourse.size());
     	for(int i = 0; i < currentCourse.size(); i++) {
     		System.out.println(currentCourse.get(i));
-    	}
+    	}**/
     	
     	// This array will store the number of course repeats in order of sorted appearance
     	int[] arr = new int[numCourses];
@@ -104,12 +188,14 @@ public class ScheduleMaker {
     	for(int i = 0; i < numCourses; i++) {
     		// Go through the multiple times of that class
     		for(int j = 0; j < arr[i]; j++) {
+    			System.out.println(currentCourse.get(i + j));
     			// Add the first class
     			if(i == 0) {
     				finalCourseList.add(currentCourse.get(i+j));
     			} else {
-    				System.out.println(currentCourse.get(i + j));
-    				if(finalCourseList.get( i - 1 ).conflicts(currentCourse.get(i + j))) {
+    				// Add other classes
+    				
+    				if(finalCourseList.get( i - 1 ).conflicts(currentCourse.get(i + j + arr[i - 1]))) {
     					// Conflict go to next option
     					if(j == arr[i] - 1) {
     						// If on the last option of a class and can't add it ERROR
@@ -117,7 +203,9 @@ public class ScheduleMaker {
     					}
     					continue;
     				} else {
-    					finalCourseList.add(currentCourse.get(i + j));
+    					
+    					System.out.println(i);
+    					finalCourseList.add(currentCourse.get(i + j + arr[i - 1]));
     					break;
     				}
     			}
